78e66f76a9ce1e07bd409a8404671318b64eafd1
madeinouweland <loek2015@outlook.com>
2016-07-31 18:18:14 +0200

    go!

---
+++ JSONSerializingNative/JSONSerializer.cs
@@ -0,0 +1,31 @@
+﻿// Made with ❤ in Berlin by Loek van den Ouweland
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace JSONSerializingNative
+{
+    public class JSONSerializer<T>
+    {
+        public string Serialize(T o)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                serializer.WriteObject(ms, o);
+                var reader = new StreamReader(ms);
+                ms.Position = 0;
+                return reader.ReadToEnd();
+            }
+        }
+
+        public T Deserialize(string json)
+        {
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                var deserializer = new DataContractJsonSerializer(typeof(T));
+                return (T)deserializer.ReadObject(ms);
+            }
+        }
+    }
+}
---
+++ JSONSerializingNative/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Windows.UI.Xaml;
+
+namespace JSONSerializingNative
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var json = "{\"Employees\":[{\"Age\":35,\"Name\":\"Vera\"},{\"Age\":41,\"Name\":\"Chuck\"},{\"Age\":26,\"Name\":\"Dave\"}],\"Name\":\"Toverstudio\"}";
+
+            var serializer = new JSONSerializer<Company>();
+            var company = serializer.Deserialize(json);
+
+            company.Name = "Aliens Bring Change";
+
+            var json2 = serializer.Serialize(company);
+            Debug.WriteLine(json2);
+        }
+    }
+}
